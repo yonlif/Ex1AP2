92c3504a43862ea020d8d523e73eeb548de2cbd7
Yonatan Lifshitz <yonlif@gmail.com>
2019-03-23 21:27:48 +0200

    Added given files

---
+++ Ex1AP2/MissionContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+  
+    public class FunctionsContainer
+    {
+        Dictionary<string, Func<double, double>> string2Function = new Dictionary<string, Func<double, double>>();
+
+        public Func<double, double> this[string key]
+        {
+            get => string2Function[key];
+            set => string2Function[key] = value;
+        }
+        
+        
+
+    }
+}
---
+++ Ex1AP2/SingleMission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class SingleMission : IMission
+    {
+        public event EventHandler<double> OnCalculate;
+        public string Name { get; }
+        public string Type { get; }
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

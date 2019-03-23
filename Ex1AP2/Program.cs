d60e84a39552005f2ae8351ee3741cd5bccb9445
Yonatan Lifshitz <yonlif@gmail.com>
2019-03-23 21:14:14 +0200

    Initial commit

---
+++ Ex1AP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex1AP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
